433e1799ee24d7c67880d29278cf8f45cb499b7a
jemmy <flashyjotebele@gmail.com>
2022-11-10 04:01:44 -1100

    jemmy --added form validation

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const firstName = document.getElementById("first-name");
+const lastName = document.getElementById("last-name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submit = document.getElementById("submit");
+
+let formValidation = (id) => {
+  if (id.value.trim().length === 0) {
+    const paragraph = id.nextElementSibling;
+    id.classList.add("bg-image");
+    paragraph.classList.remove("hidden");
+  } else {
+    const paragraph = id.nextElementSibling;
+    id.classList.remove("bg-image");
+    paragraph.classList.add("hidden");
+  }
+};
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  formValidation(firstName);
+  formValidation(lastName);
+  formValidation(email);
+  formValidation(password);
+});
